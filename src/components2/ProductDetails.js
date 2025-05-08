e2796cfa18c22fdeab6ecdec7424ef60adef0c68
Harshita choudhary <harshitachoudhary52516@gmail.com>
2025-05-08 03:55:54 +0000

    commited

--- src/components2/ProductDetails.js
+++ src/components2/ProductDetails.js
@@ -42,12 +42,34 @@ return(
 
             <p style={{color:"#797c8d", margin:'5px 0 0 25px',fontSize:'14px'}}>MRP <b>₹439</b> (incl. of all taxes)</p>
             <div style={{ width:'450px',height:'60px',backgroundColor:'#f9f9fd',margin:'20px 0 0 25px',borderRadius:'6px', border:'1px solid #f0f4f9',display:'flex'}}>
-            <div></div>
+            <div style={{width:'50px',height:'50px'}}></div>
             <div>
-                <p></p>
-                <p></p>
+                
+                <p style={{fontSize:'12px',margin:'16px 0 0 0'}}>Get at 89 with coupon offers</p>
+                <p style={{fontSize:'12px', color:'#fa3778',fontWeight:'bold'}}>View all offers</p>
+              
             </div>
-            </div>
+            <svg
+            fill="none"
+            height="16"
+            viewBox="0 0 17 16"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ height: '1.2rem', width: '1.2rem', color: '#FA3778', margin:'22px 0 0 220px',}}
+          >
+            <path
+              d="M7 4.5L10.5 8L7 11.5"
+              stroke="#FA3778"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+          </svg>
+
+          
+            </div> 
+            <p style={{color:'#5a6477',fontSize:'16px',fontWeight:'bold',margin:'20px 0 0 25px'}}>Coupon & Offers</p>
+            <hr style={{ width:'70%',backgroundColor:''}}/>
         </div>
     </div>
 </div>
