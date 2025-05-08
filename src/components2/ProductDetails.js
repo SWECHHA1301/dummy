a75899af59308dc55896dd59facf448248ba9286
Harshita choudhary <harshitachoudhary52516@gmail.com>
2025-05-08 05:15:02 +0000

    updated

--- src/components2/ProductDetails.js
+++ src/components2/ProductDetails.js
@@ -6,9 +6,57 @@ return(
     <div className="Product">
         <div>
     <div className="Nav">
-        <span><a href="https://www.zeptonow.com/">Home</a></span>
-        <span><a href="|#">Tea&Coffee</a></span>
-        <span><a>Vietnamese Cold Coffee</a></span>
+        <span><a href="https://www.zeptonow.com/">Home</a>
+        <svg
+            fill="none"
+            height="16"
+            viewBox="0 0 17 16"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ height: '1rem', width: '1rem', color: 'black',}}
+          >
+            <path
+              d="M7 4.5L10.5 8L7 11.5"
+              stroke="#FA3778"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+          </svg></span>
+        <span><a href="|#">Tea&Coffee</a>
+        <svg
+            fill="none"
+            height="16"
+            viewBox="0 0 17 16"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ height: '1rem', width: '1rem', color: 'black', }}
+          >
+            <path
+              d="M7 4.5L10.5 8L7 11.5"
+              stroke="#FA3778"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+          </svg></span>
+        <span><a>Vietnamese Cold Coffee</a>
+        <svg
+            fill="none"
+            height="16"
+            viewBox="0 0 17 16"
+            width="17"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ height: '1rem', width: '1rem', color: 'black',}}
+          >
+            <path
+              d="M7 4.5L10.5 8L7 11.5"
+              stroke="#FA3778"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+          </svg></span>
     </div>
     <div className="imageDetail">
       <div className="itemimage1">
