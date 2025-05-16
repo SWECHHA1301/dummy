f1c8a79c6a471417cae272972e36f8f09d3bc125
Aman Singh <amansingh6131@gmail.com>
2025-05-16 19:19:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './home/components/Navbar';
-import Home from './home/components/Home';
-import ProfileSection from './profile/ProfileSection';
-import Cart from './cart/Cart';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./home/components/Navbar";
+import Home from "./home/components/Home";
+import ProfileSection from "./profile/ProfileSection";
+import Cart from "./cart/Cart";
+import ProductDetails from "./product/ProductDetails";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />               
+        <Route path="/" element={<Home />} />
         <Route path="/profile" element={<ProfileSection />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
