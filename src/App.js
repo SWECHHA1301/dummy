2d988452de720d77110fb58cd03586fcc8014dcf
SWECHHA1301 <swechhasharma231@gmail.com>
2025-05-18 13:57:05 +0000

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from "./home/components/Navbar";
-// import Home from "./home/components/Home";
-// import ProfileSection from "./profile/ProfileSection";
-// import Cart from "./cart/Cart";
-// import ProductDetails from "./product/ProductDetails";
-// import Order from './profile/components/Order';
-// import OrderDetails from './orders/OrderDetails';
-// import SearchPage from "./home/components/SearchPage";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/profile" element={<ProfileSection />} />
-//         <Route path="/cart" element={<Cart />} />
-//         <Route path="/product/:productId" element={<ProductDetails />} />
-//         <Route path="/orders" element={<Order />} />                  
-//         <Route path="/order-details" element={<OrderDetails />} />
-//         <Route path="/search-bar" element={<SearchPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./home/components/Navbar";
+import Home from "./home/components/Home";
+import ProfileSection from "./profile/ProfileSection";
+import Cart from "./cart/Cart";
 import ProductDetail from "./ProductDetails/ProductDetail";
-import './ProductDetails/product.css'
-export default function App() {
+import Order from './profile/components/Order';
+import OrderDetails from './orders/OrderDetails';
+import SearchPage from "./home/components/SearchPage";
+import './home/components/app.css'
+
+function App() {
   return (
-    <div>
-    <ProductDetail/> 
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfileSection />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/orders" element={<Order />} />                  
+        <Route path="/order-details" element={<OrderDetails />} />
+        <Route path="/search-bar" element={<SearchPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
+
+// import React from 'react'
+// import ProductDetail from "./ProductDetails/ProductDetail";
+// import './ProductDetails/product.css'
+// import LoginForm from './login/LoginForm'
+// import LoginContinue from './login/LoginContinue'
+// export default function App() {
+//   return (
+//     <div>
+//     <ProductDetail/> 
+//    <LoginForm/>
+//     <LoginContinue/> 
+//     </div>
+//   )
+// }
 
