121e368ad370727e4a57a8a1c71ac9475c4f22f0
Aman Singh <amansingh6131@gmail.com>
2025-05-19 17:12:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,32 +5,31 @@ import Home from "./home/components/Home";
 import ProfileSection from "./profile/ProfileSection";
 import Cart from "./cart/Cart";
 import ProductDetail from "./ProductDetails/ProductDetail";
-import Order from './profile/components/Order';
-import OrderDetails from './orders/OrderDetails';
+import Order from "./profile/components/Order";
+import OrderDetails from "./orders/OrderDetails";
 import SearchPage from "./home/components/SearchPage";
-import './home/components/app.css';
+import "./home/components/app.css";
 
 function App() {
   const [searchTerm, setSearchTerm] = useState(""); // ✅ Define state here
 
   return (
     <Router>
-      <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} /> {/* ✅ Pass props */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProfileSection />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product/:productId" element={<ProductDetail />} />
-        <Route path="/orders" element={<Order />} />
-        <Route path="/order-details" element={<OrderDetails />} />
-       <Route path="/search-bar" element={<SearchPage searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
-      </Routes>
+      <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />{" "}
+      {/* ✅ Pass props */}
+      <Route path="/" element={<Home />} />
+      <Route path="/profile/:subMenu" element={<ProfileSection />} />
+      <Route path="/profile/order/:orderId" element={<ProfileSection />} />
+      <Route path="/profile/*" element={<ProfileSection />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/product/:productId" element={<ProductDetail />} />
+      <Route path="/orders" element={<Order />} />
+      <Route path="/search-bar" element={<SearchPage />} />
     </Router>
   );
 }
 
 export default App;
-
 
 // import React from 'react'
 // import ProductDetail from './ProductDetails/ProductDetail'
